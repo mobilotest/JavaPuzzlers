287dc925f018a86d2da1ae11d6b8f02b776dd9cd
Sergey Odintsov <mobilotest@gmail.com>
2022-06-07 11:30:37 -0700

    oddity

---
+++ src/Oddity.java
@@ -0,0 +1,9 @@
+public class Oddity {
+    public static void main(String[] args) {
+        System.out.println(isOdd(2));
+    }
+
+    public static boolean isOdd(int i){
+        return i%2 == 1;
+    }
+}
